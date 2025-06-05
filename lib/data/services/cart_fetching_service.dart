d01dae4f282313f75b89a80a8de00289638a26ff
yeabsera <yeab.dev@gmail.com>
2025-06-06 00:45:33 +0300

    feat: modify services

--- lib/data/services/cart_fetching_service.dart
+++ lib/data/services/cart_fetching_service.dart
@@ -1,13 +1,39 @@
+import 'package:dio/dio.dart';
 import 'package:gebeya/domain/models/cart.dart';
 
 class CartFetchingService {
-  Future<List<Cart>?> fetchAllCarts() async {
-    //TODO: implement fetchAllCarts()
-    throw UnimplementedError();
+  final Dio dio;
+  final String _cartEndpoint = "carts";
+
+  CartFetchingService({required this.dio});
+
+  Future<List<Cart>> fetchAllCarts() async {
+    try {
+      final response = await dio.get(_cartEndpoint);
+
+      if (response.statusCode == 200 && response.data is List) {
+        return (response.data as List)
+            .map((cartJson) => Cart.fromJson(cartJson))
+            .toList();
+      } else {
+        throw Exception("Unexpected response format");
+      }
+    } catch (e) {
+      throw Exception("Failed to fetch carts: $e");
+    }
   }
 
-  Future<Cart?> getCartByID(int id) async {
-    // TODO: implement getCartById()
-    throw UnimplementedError();
+  Future<Cart> getCartByID(int id) async {
+    try {
+      final response = await dio.get("$_cartEndpoint/$id");
+
+      if (response.statusCode == 200 && response.data != null) {
+        return Cart.fromJson(response.data);
+      } else {
+        throw Exception("Cart not found");
+      }
+    } catch (e) {
+      throw Exception("Failed to fetch cart by ID: $e");
+    }
   }
 }
--- lib/data/services/product_fetching_service.dart
+++ lib/data/services/product_fetching_service.dart
@@ -1,13 +1,36 @@
+import 'package:dio/dio.dart';
 import 'package:gebeya/domain/models/product.dart';
 
 class ProductFetchingService {
-  Future<List<Product>?> fetchAllProducts() {
-    // TODO: Implement fetchAllProducts()
-    throw UnimplementedError();
+  const ProductFetchingService({required this.dio});
+  final String _productEndpoint = 'products';
+  final Dio dio;
+
+  Future<List<Product>> fetchAllProducts() async {
+    try {
+      final response = await dio.get(_productEndpoint);
+      if (response.statusCode == 200 && response.data is List) {
+        return (response.data as List)
+            .map((productJson) => Product.fromJson(productJson))
+            .toList();
+      } else {
+        throw Exception("Unexpected Response Format");
+      }
+    } catch (e) {
+      throw Exception("Failed to fetch carts: $e");
+    }
   }
 
-  Future<Product?> getProductByID(int id) {
-    //TODO: Implement getProductByID()
-    throw UnimplementedError();
+  Future<Product> getProductByID(int id) async {
+    try {
+      final response = await dio.get("$_productEndpoint/$id");
+      if (response.statusCode == 200 && response.data != null) {
+        return Product.fromJson(response.data);
+      } else {
+        throw Exception("Unexpected Response Format");
+      }
+    } catch (e) {
+      throw Exception("Failed to fetch product: $e");
+    }
   }
 }
